1a21f11e94f6c204114ad281544829696b5b12ca
Levent C. Yilmaz <leventcanyilmaz@proton.me>
2025-12-04 01:00:34 +0300

    xxx

--- keyboards/sofle/keymaps/can/keymap.c
+++ keyboards/sofle/keymaps/can/keymap.c
@@ -324,14 +324,14 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             } else {
                 tap_code(KC_BRID);
             }
-        } 
+        }
         else {
             if (clockwise) {
                 tap_code(KC_VOLU);
             } else {
                 tap_code(KC_VOLD);
             }
-        } 
+        }
     } else if (index == 1) {
         if (clockwise) {
             tap_code(KC_PGDN);
